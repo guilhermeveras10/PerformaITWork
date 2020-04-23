0672a1f5989c871a876d6571770a5f078424ce22
Guilherme. Duarte <guilherme.duarte@performait.com>
2020-04-23 15:10:07 -0300

    Initial Commit

---
+++ PerformTwiter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PerformTwiter
+//
+//  Created by Guilherme. Duarte on 23/04/20.
+//  Copyright © 2020 Guilherme Duarte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
